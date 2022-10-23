c6f72f816f5a109f6cc30fdcfec73df2afa61279
repcaps <116498605+repcaps@users.noreply.github.com>
2022-10-23 22:35:37 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,32 @@
+<style>
+    .car-cover{
+        width:10em;
+    }
+    .car-item .col-auto{
+        max-width: calc(100% - 12em) !important;
+    }
+    .car-item:hover{
+        transform:translate(0, -4px);
+        background:#a5a5a521;
+    }
+    .banner-img-holder{
+        height:25vh !important;
+        width: calc(100%);
+        overflow: hidden;
+    }
+    .banner-img{
+        object-fit:scale-down;
+        height: calc(100%);
+        width: calc(100%);
+        transition:transform .3s ease-in;
+    }
+    .car-item:hover .banner-img{
+        transform:scale(1.3)
+    }
+    .welcome-content img{
+        margin:.5em;
+    }
+</style>
+
+
+<!--dito mag code sa search and filter
